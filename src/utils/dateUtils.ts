0df098d6da36c2ed2228fd6208997db418ae2757
beming-dev <mingfordev@gmail.com>
2023-05-14 11:06:10 +0900

    Update timezone

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -8,14 +8,15 @@ dayjs.extend(timezone);
 
 const TZ_SEOUL = "Asia/Seoul";
 
+dayjs.tz.setDefault(TZ_SEOUL);
+
 export const now = () => dayjs().tz(TZ_SEOUL);
 
 export const strToDate = (dateStr: string) => {
-  dayjs().tz(TZ_SEOUL);
-  return dayjs(dateStr, "YYYY-MM-DD").tz(TZ_SEOUL);
+  return dayjs(dateStr).tz();
 };
 
-const dateParser = (dateStr: string) => {
-  const dayjsDate = strToDate(dateStr);
-  const date = dayjsDate.toDate();
-};
+// const dateParser = (dateStr: string) => {
+//   const dayjsDate = strToDate(dateStr);
+//   const date = dayjsDate.toDate();
+// };
