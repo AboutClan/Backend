8bad966cf3488447844d505c427004707d78d572
beming-dev <mingfordev@gmail.com>
2023-05-14 10:56:32 +0900

    Add seioult time

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,16 +2,18 @@ import dayjs from "dayjs";
 import dotenv from "dotenv";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+const seoulTime = require("dayjs/plugin/timezone");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
-// dayjs.extend(utc);
-// dayjs.extend(timezone);
+dayjs.extend(seoulTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-// dayjs.tz("2023-05-14 00:00", "Asia/Seoul");
-// dayjs.tz.setDefault();
+dayjs.tz("2023-05-14 00:00", "Asia/Seoul");
+dayjs.tz.setDefault();
 
 export const config = {
   server: {
