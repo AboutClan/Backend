7004656adc0b0329b5270b6fd5a28c5c7385cde3
beming-dev <mingfordev@gmail.com>
2023-05-22 21:48:07 +0900

    Add telephone encode process

--- src/services/registerService.ts
+++ src/services/registerService.ts
@@ -1,6 +1,7 @@
 import { JWT } from "next-auth/jwt";
 import { IRegistered, Registered } from "../db/models/registered";
 import { User } from "../db/models/user";
+import * as CryptoJS from "crypto-js";
 
 export default class RegisterService {
   private token: JWT;
@@ -8,14 +9,49 @@ export default class RegisterService {
     this.token = token as JWT;
   }
 
+  async encodeByAES56(tel: string) {
+    const key = process.env.cryptoKey;
+    if (!key) return tel;
+
+    const cipher = CryptoJS.AES.encrypt(tel, CryptoJS.enc.Utf8.parse(key), {
+      iv: CryptoJS.enc.Utf8.parse(""),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+    return cipher.toString();
+  }
+
+  async decodeByAES256(encodedTel: string) {
+    const key = process.env.cryptoKey;
+    if (!key) return encodedTel;
+
+    const cipher = CryptoJS.AES.decrypt(
+      encodedTel,
+      CryptoJS.enc.Utf8.parse(key),
+      {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC,
+      }
+    );
+    return cipher.toString(CryptoJS.enc.Utf8);
+  }
+
   async register(subRegisterForm: Omit<IRegistered, "uid" | "profileImage">) {
+    const { telephone } = subRegisterForm;
+    const encodedTel = await this.encodeByAES56(telephone);
+    if (encodedTel === telephone) throw new Error();
+
     const registerForm = {
       uid: this.token.uid,
       profileImage: this.token.picture,
       ...subRegisterForm,
+      telephone: encodedTel,
     };
-    await Registered.create(registerForm);
-    // await Registered.collection.insertOne(registerForm);
+    await Registered.findOneAndUpdate({ uid: this.token.uid }, registerForm, {
+      upsert: true,
+      new: true,
+    });
 
     return;
   }
