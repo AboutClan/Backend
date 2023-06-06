e5b64e2b229f51d73cd4e8a6fe03ca52cc30c575
beming-dev <mingfordev@gmail.com>
2023-06-06 09:14:16 +0900

    Update log api

--- src/services/logService.ts
+++ src/services/logService.ts
@@ -8,23 +8,6 @@ export default class LogService {
   }
 
   async getLog(type: string) {
-    // try {
-    //   const logs = await MongoDB.find(
-    //     {},
-    //     { limit: 10, sort: { timestamp: -1 } }
-    //   ).exec();
-    //   return logs;
-    // } catch (error) {
-    //   console.error("로그 가져오기 실패:", error);
-    //   return null;
-    // }
-
-    // const logs = logger.getLogsFromMongoDB();
-
-    // const logs = winston.loggers.get();
-
-    // const logs = Log.find({ meta: type });
-
     const logs = await Log.collection
       .aggregate([
         {
@@ -35,8 +18,42 @@ export default class LogService {
             },
           },
         },
+        {
+          $project: {
+            _id: false,
+            timestamp: "$timestamp",
+            message: "$message",
+            meta: "$meta",
+          },
+        },
       ])
       .toArray();
+
+    return logs;
+  }
+
+  async getAllLog(type: string) {
+    const logs = await Log.find(
+      { "meta.type": type },
+      "-_id timestamp message meta"
+    );
+    // const logs = await Log.collection
+    //   .aggregate([
+    //     {
+    //       $match: {
+    //         "$meta.type": type,
+    //       },
+    //     },
+    //     {
+    //       $project: {
+    //         _id: false,
+    //         timestamp: "$timestamp",
+    //         message: "$message",
+    //         meta: "$meta",
+    //       },
+    //     },
+    //   ])
+    //   .toArray();
 
     return logs;
   }
