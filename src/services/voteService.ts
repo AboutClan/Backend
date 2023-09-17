6b2620f660e71d9604fd7558e04e0e86c50124cb
SeungJu <84257439+banana970@users.noreply.github.com>
2023-09-17 16:38:44 +0900

    filteredVote에 다른 지역간의 공통 스터디 장소 필터링 추가

--- src/services/voteService.ts
+++ src/services/voteService.ts
@@ -178,16 +178,18 @@ export default class VoteService {
   async getFilteredVote(date: any, location: string) {
     try {
       const filteredVote = await this.getVote(date);
-
       filteredVote.participations = filteredVote?.participations.filter(
-        (participation) => participation.place?.location === location
+        (participation) =>
+          participation.place?.location === location ||
+          (location === "안양" &&
+            participation.place?.id === "640c271121863deff358f459")
       );
-
       return filteredVote;
     } catch (err) {
       throw new Error();
     }
   }
+  /** 인접한 지역들은 공통된 스터디 장소를 추가하고자 함. 해당 지역들 따로 저장해서 체크하면 좋을 거 같은데 일단은 하드코딩으로 해둘게요. */
 
   async setVote(date: any, studyInfo: IVoteStudyInfo) {
     try {
