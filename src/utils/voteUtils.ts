15d1c3651261af7406d7dc0d10205171a170ec6e
SeungJu <84257439+banana970@users.noreply.github.com>
2023-10-17 14:57:05 +0900

    Vote 최신순 정렬

--- src/utils/voteUtils.ts
+++ src/utils/voteUtils.ts
@@ -1,8 +1,10 @@
 import { Vote } from "../db/models/vote";
 
 export const findOneVote = async (date: Date) =>
-  await Vote.findOne({ date }).populate([
-    "participations.place",
-    "participations.attendences.user",
-    "participations.absences.user",
-  ]);
+  await Vote.findOne({ date })
+    .sort({ date: -1 })
+    .populate([
+      "participations.place",
+      "participations.attendences.user",
+      "participations.absences.user",
+    ]);
