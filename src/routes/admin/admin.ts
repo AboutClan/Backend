5b8f6c1b94324a90fdceb5e414d62d1b876c5d76
SeungJu <84257439+banana970@users.noreply.github.com>
2023-11-22 16:19:54 +0900

    .

--- src/routes/admin/admin.ts
+++ src/routes/admin/admin.ts
@@ -6,10 +6,12 @@ const user = require("./user");
 const vote = require("./vote");
 const log = require("./log");
 const manage = require("./manage");
+const counter = require("./counter");
 
 router.use("/vote", vote);
 router.use("/user", user);
 router.use("/log", log);
 router.use("/manage", manage);
+router.use("/counter", counter);
 
 module.exports = router;
