1fc3b695d525692db6b9fab43f38cff8350015a9
SeungJu <84257439+banana970@users.noreply.github.com>
2023-12-18 16:01:46 +0900

    .

--- src/services/groupStudyService.ts
+++ src/services/groupStudyService.ts
@@ -84,8 +84,7 @@ export default class GroupStudyService {
   }
   async getWaitingPerson(id: string) {
     try {
-      return id;
-      const data = await GroupStudy.findOne({ id }).populate(["waiting"]);
+      const data = await GroupStudy.findOne({ id }).populate(["waiting.user"]);
 
       return data;
     } catch (err: any) {
