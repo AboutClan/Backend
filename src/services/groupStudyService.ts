5e7d121ed09cf592f3282e81e6c9d5e298dc6fe1
SeungJu <84257439+banana970@users.noreply.github.com>
2023-12-18 18:14:23 +0900

    .

--- src/services/groupStudyService.ts
+++ src/services/groupStudyService.ts
@@ -134,20 +134,20 @@ export default class GroupStudyService {
     if (!groupStudy) throw new Error();
 
     try {
-      return groupStudy.waiting[0].user.toString();
-      groupStudy.waiting = groupStudy.waiting.filter(
-        (who) => who.user !== userId
-      );
-      if (status === "agree") {
-        groupStudy.participants.push({ user: userId, role: "member" });
-      }
+      return groupStudy.waiting[0].user.toString() === userId;
+      // groupStudy.waiting = groupStudy.waiting.filter(
+      //   (who) => who.user !== userId
+      // );
+      // if (status === "agree") {
+      //   groupStudy.participants.push({ user: userId, role: "member" });
+      // }
 
-      await groupStudy?.save();
-      return {
-        a: groupStudy.waiting[0].user,
-        b: userId,
-        c: (groupStudy.waiting[0].user as string) === userId,
-      };
+      // await groupStudy?.save();
+      // return {
+      //   a: groupStudy.waiting[0].user,
+      //   b: userId,
+      //   c: (groupStudy.waiting[0].user as string) === userId,
+      // };
     } catch (err) {
       throw new Error();
     }
