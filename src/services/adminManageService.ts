69794ee2ba9b080be75dd145bb6f8bd03eda2711
seungju <j44s11@naver.com>
2024-01-08 10:34:23 +0900

    .

--- src/services/adminManageService.ts
+++ src/services/adminManageService.ts
@@ -82,7 +82,7 @@ export default class AdminManageService {
           lastMonthEnd,
           true
         );
-
+      return participationRate;
       const notPartUsers: any[] = [];
       fUsers.forEach((user) => {
         const idx = participationRate.findIndex(
