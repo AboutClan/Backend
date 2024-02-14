d04a45a3492d5b61a37894dd777f572a0af0bb8e
beming-dev <mingfordev@gmail.com>
2024-02-14 18:19:07 +0900

    Update to initial version of image upload

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -19,46 +19,37 @@ const s3 = new AWS.S3({
   region: "ap-northeast-2",
 });
 
-const fileUpload = (path: string) => {
-  return multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: "studyabout",
-      acl: "public-read",
-      metadata: function (req: any, file: any, cb: any) {
-        cb(null, { fieldName: file.fieldname });
-      },
-      key: function (req: any, file: any, cb: any) {
-        const folder = req.body.path;
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "studyabout",
+    acl: "public-read",
+    metadata: function (req: any, file: any, cb: any) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req: any, file: any, cb: any) {
+      const folder = req.body.path;
 
-        for (let value of req.body.values()) {
-          console.log(value); // value
-        }
-        cb(null, `${folder}/${Date.now().toString()}`);
-      },
-    }),
-  });
-};
+      cb(null, `${folder}/${Date.now().toString()}`);
+    },
+  }),
+});
 
-router.post(
-  "/upload",
-  fileUpload("path").single("image"),
-  async function (req, res, next) {
-    if (!req.file) {
-      return res.status(400).json({
-        ok: false,
-        message: "사진 파일이 잘못되었습니다.",
-      });
-    }
-
-    return res.status(201).json({
-      ok: true,
-      message: "사진이 성공적으로 업로드 되엇습니다.",
-      data: {
-        image: req.file,
-      },
+router.post("/upload", upload.single("image"), async function (req, res, next) {
+  if (!req.file) {
+    return res.status(400).json({
+      ok: false,
+      message: "사진 파일이 잘못되었습니다.",
     });
   }
-);
+
+  return res.status(201).json({
+    ok: true,
+    message: "사진이 성공적으로 업로드 되엇습니다.",
+    data: {
+      image: req.file,
+    },
+  });
+});
 
 module.exports = router;
