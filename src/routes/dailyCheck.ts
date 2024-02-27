db6f742e46065d37aebeb7db02a7daadef8e05b1
seungju <j44s11@naver.com>
2024-02-27 14:17:51 +0900

    test

--- src/routes/dailyCheck.ts
+++ src/routes/dailyCheck.ts
@@ -32,7 +32,7 @@ router
     try {
       const result = await dailyCheckServiceInstance?.setDailyCheck();
       if (result) return res.status(400).json({ message: result });
-      return res.status(200).end();
+      return res.status(200).json(result);
     } catch (err: any) {
       next(err);
     }
