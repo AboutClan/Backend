513061c4196c8569a30882ef6ea5a8c6f9feb58b
Jeongjin Oh <rojay.developer@gmail.com>
2024-07-25 15:17:05 +0900

    wip: squareService and square routes

--- src/routes/square.ts
+++ src/routes/square.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import { body } from "express-validator";
 import validateCheck from "../middlewares/validator";
 import SquareService from "../services/squareService";
+import { SecretSquareCategory } from "../db/models/secretSquare";
 
 class SquareController {
   public router: Router;
@@ -47,8 +48,8 @@ class SquareController {
 
     this.router
       .route("/:squareId/poll")
-      .put(this.putPoll.bind(this))
-      .get(this.canPoll.bind(this));
+      .patch(this.patchPoll.bind(this))
+      .get(this.getCurrentPollItems.bind(this));
   }
 
   private async createSquareServiceInstance(
@@ -62,51 +63,128 @@ class SquareController {
   }
 
   private async getSquareList(req: Request, res: Response, next: NextFunction) {
+    const { category = "all" } = req.query as {
+      category?: SecretSquareCategory;
+    };
+
     try {
-      const squareList = await this.SquareServiceInstance.getSquareList();
-      res.status(200).json(squareList);
+      const squareList = await this.SquareServiceInstance.getSquareList({
+        category,
+      });
+      res.status(200).json({ squareList });
     } catch (err) {
       next(err);
     }
   }
 
   private async createSquare(req: Request, res: Response, next: NextFunction) {
-    const {
-      body: { square },
-    } = req;
+    const { square } = req.body;
 
     try {
-      await this.SquareServiceInstance?.createSquare(square);
-      res.status(200).end();
+      await this.SquareServiceInstance.createSquare(square);
+      res.status(201).end();
     } catch (err) {
       next(err);
     }
   }
 
   private async deleteSquare(req: Request, res: Response, next: NextFunction) {
-    const {
-      body: { id: squareId },
-    } = req;
+    const { squareId } = req.params;
 
     try {
-      await this.SquareServiceInstance?.deleteSquare();
-      res.status(200).end();
+      await this.SquareServiceInstance.deleteSquare(squareId);
+      res.status(204).end();
     } catch (err) {
       next(err);
     }
   }
 
-  private async getSquare(req, res, next) {}
+  private async getSquare(req: Request, res: Response, next: NextFunction) {
+    const { squareId } = req.params;
 
-  private async getSquareComments(req, res, next) {}
+    try {
+      const square = await this.SquareServiceInstance.getSquare(squareId);
+      res.status(200).json({ square });
+    } catch (err) {
+      next(err);
+    }
+  }
 
-  private async createSquareComment(req, res, next) {}
+  private async getSquareComments(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const { squareId } = req.params;
 
-  private async deleteSquareComment(req, res, next) {}
+    try {
+      const squareComments =
+        await this.SquareServiceInstance.getSquareComments(squareId);
+      res.status(200).json({ squareComments });
+    } catch (err) {
+      next(err);
+    }
+  }
 
-  private async putPoll(req, res, next) {}
+  private async createSquareComment(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const { body: comment } = req;
+    try {
+      await this.SquareServiceInstance.createSquareComment(comment);
+      res.status(201).end();
+    } catch (err) {
+      next(err);
+    }
+  }
 
-  private async canPoll(req, res, next) {}
+  private async deleteSquareComment(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const { commentId } = req.params;
+    try {
+      await this.SquareServiceInstance.deleteSquareComment(commentId);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  private async patchPoll(req: Request, res: Response, next: NextFunction) {
+    const { squareId } = req.params;
+    const { uid, pollItems = [] } = req.body;
+
+    try {
+      await this.SquareServiceInstance.patchPoll({ squareId, uid, pollItems });
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  private async getCurrentPollItems(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const { squareId } = req.params;
+    const { uid } = req.body;
+
+    try {
+      const currentPollItems =
+        await this.SquareServiceInstance.getCurrentPollItems({
+          squareId,
+          uid,
+        });
+      res.status(200).end({ pollItems: currentPollItems });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 const squareController = new SquareController();
--- src/services/squareService.ts
+++ src/services/squareService.ts
@@ -1,37 +1,160 @@
-import { SecretSquare } from "../db/models/secretSquare";
+import { Comment } from "../db/models/comment";
+import {
+  SecretSquareCategory,
+  SecretSquare,
+  SecretSquareType,
+} from "../db/models/secretSquare";
+
+// TODO need to resolve type error
+
+interface Square {
+  category: SecretSquareCategory;
+  title: string;
+  content: string;
+  type: SecretSquareType;
+  poll: { name: string }[];
+  canMultiple: boolean;
+  author: string;
+}
 
 export default class SquareService {
   constructor() {}
 
-  async getSquareList() {
-    try {
-      const squareData = await SecretSquare.find();
-      return squareData;
-    } catch (err: any) {
-      throw new Error(err);
+  async getSquareList({
+    category,
+  }: {
+    category: SecretSquareCategory | "all";
+  }) {
+    if (category === "all") {
+      return await SecretSquare.find();
+    }
+    return await SecretSquare.find({ category });
+  }
+
+  async createSquare(square: Square) {
+    const {
+      category,
+      title,
+      content,
+      type: squareType,
+      author,
+      poll: pollItems,
+      canMultiple,
+    } = square;
+
+    if (squareType === "poll") {
+      await SecretSquare.create({
+        category,
+        title,
+        content,
+        author,
+        poll: {
+          pollItems,
+          canMultiple,
+        },
+      });
+    } else {
+      await SecretSquare.create({
+        category,
+        title,
+        content,
+        author,
+      });
     }
   }
 
-  async createSquare(data: any) {
-    try {
-      await SecretSquare.create(data);
-      return;
-    } catch (err: any) {
-      throw new Error(err);
-    }
+  async deleteSquare(squareId: string) {
+    await SecretSquare.findByIdAndDelete(squareId);
   }
 
-  async deleteSquare() {}
+  async getSquare(squareId: string) {
+    const square = await SecretSquare.findById(squareId);
+    return square;
+  }
 
-  async getSquare() {}
+  async getSquareComments(squareId: string) {
+    const comments = await Comment.find({ squareId });
+    return comments;
+  }
 
-  async getSquareComments() {}
+  async createSquareComment({
+    user,
+    comment,
+    squareId,
+  }: {
+    user: string;
+    comment: string;
+    squareId: string;
+  }) {
+    await Comment.create({ user, comment, squareId });
+  }
 
-  async createSquareComment() {}
+  async deleteSquareComment(commentId: string) {
+    await Comment.findByIdAndDelete(commentId);
+  }
 
-  async deleteSquareComment() {}
+  async patchPoll({
+    squareId,
+    uid,
+    pollItems,
+  }: {
+    squareId: string;
+    uid: string;
+    pollItems: string[];
+  }) {
+    const secretSquare = await SecretSquare.findById(squareId);
 
-  async putPoll() {}
+    if (!secretSquare) {
+      throw new Error("SecretSquare not found");
+    }
 
-  async canPoll() {}
+    if (pollItems.length === 0) {
+      secretSquare.poll.pollItems.forEach((pollItem) => {
+        const index = pollItem.users.indexOf(uid);
+        if (index > -1) {
+          pollItem.users.splice(index, 1);
+        }
+      });
+    } else {
+      secretSquare.poll.pollItems.forEach((pollItem) => {
+        const index = pollItem.users.indexOf(uid);
+        if (index > -1) {
+          pollItem.users.splice(index, 1);
+        }
+      });
+
+      pollItems.forEach((pollItemId) => {
+        const pollItem = secretSquare.poll.pollItems.find(
+          (pollItem) => pollItem._id === pollItemId,
+        );
+        if (pollItem) {
+          pollItem.users.push(uid);
+        }
+      });
+    }
+
+    await secretSquare.save();
+  }
+
+  async getCurrentPollItems({
+    squareId,
+    uid,
+  }: {
+    squareId: string;
+    uid: string;
+  }) {
+    const secretSquare = await SecretSquare.findById(squareId);
+
+    if (!secretSquare) {
+      throw new Error("SecretSquare not found");
+    }
+    const pollItems: string[] = [];
+
+    secretSquare.poll.pollItems.forEach((pollItem) => {
+      if (!pollItem.users.includes(uid)) return;
+      pollItems.push(pollItem._id);
+    });
+
+    return pollItems;
+  }
 }
