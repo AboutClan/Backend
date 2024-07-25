2cb62be12a90e5464637feb00634fb171cbc1f6a
LSJ <j44s11@naver.com>
2024-07-25 21:55:17 +0900

    fix error

--- src/services/groupStudyService.ts
+++ src/services/groupStudyService.ts
@@ -158,7 +158,7 @@ export default class GroupStudyService {
 
   async getUserParticipatingGroupStudy() {
     const userParticipatingGroupStudy = await GroupStudy.find({
-      participants: { $elemMatch: { $eq: this.token.id } },
+      "participants.user": this.token.id,
     })
       .populate({
         path: "organizer",
