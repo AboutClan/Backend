c695d98223371049e8b80448d654a4b951a3d320
beming-dev <mingfordev@gmail.com>
2024-08-19 15:32:04 +0900

    Rollback notification time

--- src/schedule/schedule.ts
+++ src/schedule/schedule.ts
@@ -9,9 +9,9 @@ const schedule = require("node-schedule");
 export function sendNoti() {
   try {
     const rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = [1, 2, 3, 5, 6]; // 월, 화, 수, 금, 토
-    rule.hour = 15; // 오후 6시
-    rule.minute = 30;
+    rule.dayOfWeek = [2, 3, 5, 6]; // 월, 화, 수, 금, 토
+    rule.hour = 18; // 오후 6시
+    rule.minute = 0;
     rule.tz = "Asia/Seoul"; // 한국 시간대
 
     const webPushServiceInstance = new WebPushService();
