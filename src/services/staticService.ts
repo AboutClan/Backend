a2397caacd1f82d2297bb0891f6ec849a687439f
beming-dev <mingfordev@gmail.com>
2024-09-11 22:44:02 +0900

    Update static service

--- src/services/staticService.ts
+++ src/services/staticService.ts
@@ -19,6 +19,31 @@ export default class StaticService {
     else return false;
   }
 
+  // 공통적으로 사용되는 aggregation 함수
+  async aggregateLogs(message: string, groupField: string, period: Date) {
+    return await Log.collection
+      .aggregate([
+        {
+          $match: {
+            message: message, // message 필터링
+            timestamp: {
+              $gte: period, // 한 달 이내의 timestamp 필터링
+            },
+          },
+        },
+        {
+          $group: {
+            _id: `$meta.${groupField}`, // meta.uid별로 그룹화
+            count: { $sum: 1 }, // 각 그룹의 개수를 세어 count에 저장
+          },
+        },
+        {
+          $sort: { count: -1 }, // count 기준으로 내림차순 정렬
+        },
+      ])
+      .toArray();
+  }
+
   async getUserInSameLocation(month: number = 1) {
     const manager = await User.findById(this.token.id);
     if (!manager) throw new DatabaseError("no user");
@@ -32,84 +57,44 @@ export default class StaticService {
     const oneMonthAgo = new Date(now);
     oneMonthAgo.setMonth(now.getMonth() - month);
 
-    //번개 모임
-    //소모임 가입
+    const selfStudy = await this.aggregateLogs(
+      "개인 스터디 인증",
+      "uid",
+      oneMonthAgo,
+    );
+    const attend = await this.aggregateLogs("스터디 출석", "uid", oneMonthAgo);
+    const gather = await this.aggregateLogs(
+      "번개 모임 참여",
+      "uid",
+      oneMonthAgo,
+    );
+    const group = await this.aggregateLogs("소모임 가입", "uid", oneMonthAgo);
 
-    //개인 스터디
-    const selfStudy = await Log.collection
-      .aggregate([
-        {
-          $match: {
-            message: "개인 스터디 인증", // message 필터링
-            timestamp: {
-              $gte: oneMonthAgo, // 한 달 이내의 timestamp 필터링
-            },
-          },
-        },
-        {
-          $group: {
-            _id: "$meta.uid", // meta.uid별로 그룹화
-            selfStudyCount: { $sum: 1 }, // 각 그룹의 개수를 세어 count에 저장
-          },
-        },
-        {
-          $sort: { count: -1 }, // count 기준으로 내림차순 정렬
-        },
-      ])
-      .toArray();
-
-    //스터디 출석
-    const attend = await Log.collection
-      .aggregate([
-        {
-          $match: {
-            message: "일일 출석", // message 필터링
-            timestamp: {
-              $gte: oneMonthAgo, // 한 달 이내의 timestamp 필터링
-            },
-          },
-        },
-        {
-          $group: {
-            _id: "$meta.uid", // meta.uid별로 그룹화
-            attendCount: { $sum: 1 }, // 각 그룹의 개수를 세어 count에 저장
-          },
-        },
-        {
-          $sort: { count: -1 }, // count 기준으로 내림차순 정렬
-        },
-      ])
-      .toArray();
-
-    const updatedArr2 = selfStudy
-      .filter((item1) => {
-        // matchedItem이 존재하는 경우에만 필터 통과
-        return usersInSameLocation.some((item2) => item2.uid == item1._id);
-      })
-      .map((item1) => {
-        const matchedItem = usersInSameLocation.find(
-          (item2) => item2.uid == item1._id,
-        );
-        if (matchedItem) {
-          return { ...item1, _id: matchedItem.name };
-        }
+    const mapUserData = (
+      userArray: any[],
+      logArray: any[],
+      logField: string,
+    ) => {
+      return userArray.map((user) => {
+        const matched = logArray.find((log) => user.uid == log._id);
+        const userObj =
+          typeof user.toJSON === "function" ? user.toJSON() : user;
+        if (matched) return { ...userObj, [logField]: matched.count };
+        return { ...userObj, [logField]: 0 };
       });
+    };
 
-    let updatedArr = usersInSameLocation.map((user) => {
-      const matched = selfStudy.find((info) => user.uid == info._id);
-      if (matched)
-        return { ...user.toJSON(), selfStudyCnt: matched.selfStudyCount };
-      return { ...user.toJSON(), selfStudyCnt: 0 };
-    });
-
-    updatedArr = updatedArr.map((user) => {
-      const matched = attend.find((info) => user.uid == info._id);
-      if (matched) return { ...user, attendCnt: matched.attendCount };
-      return { ...user, attendCnt: 0 };
-    });
+    let updatedArr = mapUserData(
+      usersInSameLocation,
+      selfStudy,
+      "selfStudyCnt",
+    );
+    updatedArr = mapUserData(updatedArr, attend, "attendCnt");
+    updatedArr = mapUserData(updatedArr, gather, "gatherCnt");
+    updatedArr = mapUserData(updatedArr, group, "groupCnt");
 
     const cleanedArr = updatedArr.map((obj) => {
-      const { _id, uid, ...rest } = obj;
+      const { uid, ...rest } = obj;
       return rest;
     });
 
