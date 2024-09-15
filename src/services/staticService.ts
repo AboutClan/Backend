56395ce62f7e3c7dc674b4f98dca71864303dd0a
beming-dev <mingfordev@gmail.com>
2024-09-15 15:39:56 +0900

    Update static to use date and filtering

--- src/services/staticService.ts
+++ src/services/staticService.ts
@@ -20,20 +20,26 @@ export default class StaticService {
   }
 
   // 공통적으로 사용되는 aggregation 함수
-  async aggregateLogs(message: string, groupField: string, period: Date) {
+  async aggregateLogs(message: string, groupFields: string[], period: any) {
+    const groupId = groupFields.reduce((acc: any, field) => {
+      acc[field] = `$meta.${field}`;
+      return acc;
+    }, {});
+
     return await Log.collection
       .aggregate([
         {
           $match: {
             message: message, // message 필터링
             timestamp: {
-              $gte: period, // 한 달 이내의 timestamp 필터링
+              $gte: period.firstDay, // 한 달 이내의 timestamp 필터링
+              $lte: period.lastDay, // 한 달 이내의 timestamp 필터링
             },
           },
         },
         {
           $group: {
-            _id: `$meta.${groupField}`, // meta.uid별로 그룹화
+            _id: groupId, // groupFields 배열에 있는 모든 필드로 그룹화
             count: { $sum: 1 }, // 각 그룹의 개수를 세어 count에 저장
           },
         },
@@ -44,7 +50,26 @@ export default class StaticService {
       .toArray();
   }
 
-  async getUserInSameLocation(month: number = 1) {
+  getFirstAndLastDay(dateString: string = "2024-08-15") {
+    // 문자열을 Date 객체로 변환 (YYYY-MM-DD 형식)
+    const inputDate = new Date(dateString);
+
+    // 해당 달의 첫 번째 날 (해당 년, 월의 1일)
+    const firstDay = new Date(inputDate.getFullYear(), inputDate.getMonth(), 1);
+
+    // 해당 달의 마지막 날 (해당 년, 월 + 1의 0일은 이전 달의 마지막 날을 의미)
+    const lastDay = new Date(
+      inputDate.getFullYear(),
+      inputDate.getMonth() + 1,
+      0,
+    );
+
+    return { firstDay, lastDay };
+  }
+
+  async getUserInSameLocation(date: string) {
+    const dateInfo = this.getFirstAndLastDay(date);
+
     const manager = await User.findById(this.token.id);
     if (!manager) throw new DatabaseError("no user");
     const managerLocation = manager.location;
@@ -53,22 +78,24 @@ export default class StaticService {
       location: managerLocation,
     }).select("uid name");
 
-    const now = new Date();
-    const oneMonthAgo = new Date(now);
-    oneMonthAgo.setMonth(now.getMonth() - month);
-
     const selfStudy = await this.aggregateLogs(
       "개인 스터디 인증",
-      "uid",
-      oneMonthAgo,
+      ["uid"],
+      dateInfo,
     );
-    const attend = await this.aggregateLogs("스터디 출석", "uid", oneMonthAgo);
+    const attend = await this.aggregateLogs(
+      "스터디 출석",
+      ["uid", "type"],
+      dateInfo,
+    );
+    const scoreOnlyAttend = attend.filter((item) => item._id.type === "score");
+
     const gather = await this.aggregateLogs(
       "번개 모임 참여",
-      "uid",
-      oneMonthAgo,
+      ["uid"],
+      dateInfo,
     );
-    const group = await this.aggregateLogs("소모임 가입", "uid", oneMonthAgo);
+    const group = await this.aggregateLogs("소모임 가입", ["uid"], dateInfo);
 
     const mapUserData = (
       userArray: any[],
@@ -76,7 +103,7 @@ export default class StaticService {
       logField: string,
     ) => {
       return userArray.map((user) => {
-        const matched = logArray.find((log) => user.uid == log._id);
+        const matched = logArray.find((log) => user.uid == log._id.uid);
         const userObj =
           typeof user.toJSON === "function" ? user.toJSON() : user;
         if (matched) return { ...userObj, [logField]: matched.count };
@@ -89,7 +116,7 @@ export default class StaticService {
       selfStudy,
       "selfStudyCnt",
     );
-    updatedArr = mapUserData(updatedArr, attend, "attendCnt");
+    updatedArr = mapUserData(updatedArr, scoreOnlyAttend, "attendCnt");
     updatedArr = mapUserData(updatedArr, gather, "gatherCnt");
     updatedArr = mapUserData(updatedArr, group, "groupCnt");
 
