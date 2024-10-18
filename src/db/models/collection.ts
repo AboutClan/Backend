5e86e9031b63923c91cbc588b2c273c548f1f7e4
beming-dev <mingfordev@gmail.com>
2024-10-18 20:57:58 +0900

    Update collectionzodschema

--- src/db/models/collection.ts
+++ src/db/models/collection.ts
@@ -10,6 +10,7 @@ export const CollectionZodSchema = z.object({
   type: z.enum(["alphabet"]).default("alphabet"),
   collects:  z.array(z.string()),
   collectCnt: z.number(),
+  stamps:z.number()
 })
 
 export interface ICollection {
