b422ead7dfd403c0d91c312d2707bf09821eacce
beming-dev <mingfordev@gmail.com>
2024-11-05 20:00:45 +0900

    Update vote result free creteria

--- src/services/adminVoteServices.ts
+++ src/services/adminVoteServices.ts
@@ -164,7 +164,7 @@ export default class AdminVoteService {
           if (
             participation.status === "dismissed" &&
             participation.attendences &&
-            participation.attendences.length >= 1
+            participation.attendences.length > 1
           ) {
             participation.status = "free";
           }
